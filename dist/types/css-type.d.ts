d35ac4aeb413d157ac0f619eb9cdbebde0f52b00
Tooms73 <tooms73@gmail.com>
2024-09-17 09:30:40 +0200

    delete all

--- dist/types/css-type.d.ts
+++
@@ -1,1 +0,0 @@
-export type CssType = Array<string>;
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.spec.ts'], // Match spec files
-    moduleFileExtensions: ['ts', 'js'],
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-};
