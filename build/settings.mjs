541ac9cb5b08427f7c7bd094cd12bd994332274c
roman.fadeev <roman.fadeev@acquia.com>
2024-10-23 15:33:32 +0200

    Removed esm

--- build/settings.mjs
+++ build/settings.mjs
@@ -4,7 +4,6 @@ export function createBuildSettings(options) {
     entryPoints: ['./src/index.ts'],
     tsconfig: 'tsconfig.json',
     loader: { '.ts': 'ts' }, // Use TypeScript loader
-    format: 'esm', // Output format is ES module
     bundle: true,
     ...options
   };
