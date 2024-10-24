b9b41dc73bd3b40065a4df2fffc67b927d405093
Sorin Fratila <sorin.fratila@acquia.com>
2024-10-24 10:06:57 +0200

    style: fix test path

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    testMatch: ['src/**/*.spec.ts'], // Match spec files
     testMatch: ['<rootDir>/src/**/*.spec.ts'], // Match spec files
     moduleFileExtensions: ['ts', 'js'],
     transform: {
