786101c498a4dd724414d08c64ab9ea93ea793ce
Ivan Vlasov <ivan.vlasov@acquia.com>
2024-12-05 14:40:33 +0100

    Add test for lib import

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+const lib = fs.readFileSync('./dist/index.js', { encoding: 'utf8'});
+
+describe('Main lib checks', () => {
+    it('exports DataCollector and PageBuilder', () => {
+        const matchResult = lib.match(/export{[a-zA-Z\d]{1,5} as DataCollector,l as PageBuilder}/)
+        expect(matchResult).not.toBe(null);
+    });
+});
