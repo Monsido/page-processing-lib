1d8a48364003ef36ef09b93f4bf797ef76fe6c67
Ivan Vlasov <ivan.vlasov@acquia.com>
2024-12-05 14:49:22 +0100

    Add test for the current version

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,14 @@
 import fs from 'fs';
+import { version } from '../package.json';
 const lib = fs.readFileSync('./dist/index.js', { encoding: 'utf8'});
 
 describe('Main lib checks', () => {
     it('exports DataCollector and PageBuilder', () => {
         const matchResult = lib.match(/export{[a-zA-Z\d]{1,5} as DataCollector,[a-zA-Z\d]{1,5} as PageBuilder}/)
         expect(matchResult).not.toBe(null);
+    });
+
+    it('contains the current version', () => {
+        expect(lib).toContain(version);
     });
 });
