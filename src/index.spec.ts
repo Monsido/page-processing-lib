344060a76068c7bac06dc0777efbc7a898bbc49a
Ivan Vlasov <ivan.vlasov@acquia.com>
2024-12-05 14:42:45 +0100

    onsider second export variable as well

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,7 +3,7 @@ const lib = fs.readFileSync('./dist/index.js', { encoding: 'utf8'});
 
 describe('Main lib checks', () => {
     it('exports DataCollector and PageBuilder', () => {
-        const matchResult = lib.match(/export{[a-zA-Z\d]{1,5} as DataCollector,l as PageBuilder}/)
+        const matchResult = lib.match(/export{[a-zA-Z\d]{1,5} as DataCollector,[a-zA-Z\d]{1,5} as PageBuilder}/)
         expect(matchResult).not.toBe(null);
     });
 });
