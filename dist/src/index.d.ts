2e18016ce5b4d5512a229785c5ff0e4b8ccc4bfe
roman.fadeev <roman.fadeev@acquia.com>
2025-01-07 14:25:40 +0100

    feat(OPTI-9698): Added types

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,4 +1,4 @@
 import { PageBuilder } from './page-builder/page-builder';
 import { DataCollector } from './data-collector/data-collector';
-export * from './types';
-export { PageBuilder, DataCollector };
+import { TreeType, CssType } from './types';
+export { PageBuilder, DataCollector, CssType, TreeType };
